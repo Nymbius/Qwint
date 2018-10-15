49202ae25bd1528c8e37cdd30e5a49872fa4bbe2
John <39532803+Nymbius@users.noreply.github.com>
2018-10-15 21:15:50 +0200

    Create core.cmd

---
+++ engine/core.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo This command prompt is used as the connector of all other machines in this folder. DO NOT DELETE THIS FILE! If you delete this file the other files wont be able to comunicate with each other making the game unplayeble!
+echo Press any button to start.
+pause
+start tracker.cmd
