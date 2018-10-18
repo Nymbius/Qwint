e21acc26981ae5b22958f2aba056c67d57d8782e
John <39532803+Nymbius@users.noreply.github.com>
2018-10-18 21:16:50 +0200

    Update core.cmd

--- engine/core.cmd
+++ engine/core.cmd
@@ -1,5 +1,6 @@
 @echo off
 echo This command prompt is used as the connector of all other machines in this folder. DO NOT DELETE THIS FILE! If you delete this file the other files wont be able to comunicate with each other making the game unplayeble!
 echo Press any button to start.
+/start /max 
 pause
 start tracker.cmd
