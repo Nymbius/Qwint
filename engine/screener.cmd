a1d82a3ac309a7ce292881b1a36f40a235d5afb6
John <39532803+Nymbius@users.noreply.github.com>
2018-10-19 22:27:25 +0200

    Update screener.cmd

--- engine/screener.cmd
+++ engine/screener.cmd
@@ -1,6 +1,11 @@
 @echo off
-timeout 0.1 > NUL
-echo this
-timeout 0.1 > NUL
+:: timeout 0.1 > NUL
+:: echo this
+:: timeout 0.1 > NUL
 echo that
 pause
+goto Main Screen
+
+:Main Screen
+mode con: cols=80 lines=28
+############################################
