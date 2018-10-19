cfd04410765f167fefb669fb44654c5aaae71ae5
John <39532803+Nymbius@users.noreply.github.com>
2018-10-19 22:35:27 +0200

    Update screener.cmd

--- engine/screener.cmd
+++ engine/screener.cmd
@@ -2,10 +2,51 @@
 :: timeout 0.1 > NUL
 :: echo this
 :: timeout 0.1 > NUL
-echo that
-pause
+@echo off
 goto Main Screen
 
 :Main Screen
+::Color attributes are specified by TWO hex digits -- the first
+::corresponds to the background; the second the foreground.  Each digit
+::can be any of the following values:
+::
+::    0 = Black       8 = Gray
+::    1 = Blue        9 = Light Blue
+::    2 = Green       A = Light Green
+::    3 = Aqua        B = Light Aqua
+::    4 = Red         C = Light Red
+::    5 = Purple      D = Light Purple
+::    6 = Yellow      E = Light Yellow
+::    7 = White       F = Bright White
+color 01
 mode con: cols=80 lines=28
-############################################
+echo ################################################################################
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo #                                                                              #
+echo ################################################################################
+
+pause
+##########################################
